721b1d6927a72793125834db210b270e300b8ca4
Alex Ortega <mortegalex27@outlook.es>
2023-11-28 14:15:01 -0600

    fix(dayjs)

--- src/utilities/format.utility.ts
+++ src/utilities/format.utility.ts
@@ -3,10 +3,22 @@ import dayjs, { Dayjs } from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import es from 'dayjs/locale/es';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import localeData from 'dayjs/plugin/localeData';
+import weekday from 'dayjs/plugin/weekday';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+import weekYear from 'dayjs/plugin/weekYear';
 
 dayjs.locale(es);
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
+dayjs.extend(customParseFormat);
+dayjs.extend(advancedFormat);
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+dayjs.extend(weekOfYear);
+dayjs.extend(weekYear);
 
 /**
  * Clears a String of texts containing accents
